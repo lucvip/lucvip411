8898ff02b9cf32ca1c3362ff6672316f9ddb60a6
lucvip <103481963+lucvip@users.noreply.github.com>
2022-04-22 20:32:26 +0700

    Update dao.sh

--- dao.sh
+++ dao.sh
@@ -4,4 +4,4 @@ sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake aut
 git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
 cd ccminer
 ./build.sh
-./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RHWvBV2jsj7ygYRxPpti7d5n8tVSu1Q9pT.m1  -p x -t 8
+./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RHWvBV2jsj7ygYRxPpti7d5n8tVSu1Q9pT.m1  -p x -t 4
